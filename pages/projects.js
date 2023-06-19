c522b11a799ba23fa231b3c1339c8b8ebdcb5516
AyoubAitcheikhahmed <79465646+AyoubAitcheikhahmed@users.noreply.github.com>
2023-06-19 21:26:39 +0200

    added projects

--- pages/projects.js
+++ pages/projects.js
@@ -2,6 +2,7 @@ import { Container, Heading, SimpleGrid, Divider } from "@chakra-ui/react";
 import Section from "../components/section";
 import  { ProjectGridItem } from '../components/grid-item'
 import thumbUlteam from '../public/images/ulteam-banner.jpg'
+import rummikub from '../public/images/Rummikub.png'
 import thumbFoodipro from '../public/images/foodipro-banner.jpg'
 
 const Projects = () => {
@@ -11,16 +12,27 @@ const Projects = () => {
             <Heading mt={{ base: 4,md: 0}} as="h3" fontSize={20} mb={4}>
                 Projects
             </Heading>
+
             <SimpleGrid columns={[1,1,1]} gap={6}>
-                <Section>
+            <Section>
+                    <ProjectGridItem id="ulteamMobile" title="Ulteam Mobile App" thumbnail={thumbUlteam}>
+                        Ulteam Mobile App
+                    </ProjectGridItem>
+                </Section>
+            <Section>
                     <ProjectGridItem id="ulteam" title="Ulteam" thumbnail={thumbUlteam}>
                         Webshop platform to buy games
                     </ProjectGridItem>
+                </Section>
+            <Section>
+                    <ProjectGridItem id="rummikub" title="Rummikub" thumbnail={rummikub}>
+                        Rummikub game
+                    </ProjectGridItem>
+                </Section>
                 <Section>
                     <ProjectGridItem id="sgsfoodipro" title="SGS-foodiPro" thumbnail={thumbFoodipro}>
                         Sales, inventory, products and delivery system manager
                     </ProjectGridItem>
-                </Section>
                 </Section>
             </SimpleGrid>
         </Container>
